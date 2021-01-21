500a9f97f01759f24375454a6b410c0d80938c1e
a23246542 <a23246542@gmail.com>
2021-01-21 13:53:52 +0800

    創建檔案項目

---
+++ src/components/AddTodo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import clsx from 'clsx'
+
+function AddTodo() {
+    return (
+        <div className="add-todo">
+            <div className="add-todo-icon"></div>
+            <div className="add-todo-input">
+            </div>
+            <div className="add-todo-action"></div>
+        </div>
+    )
+}
+
+export default AddTodo
